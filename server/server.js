02f884e9568358801d01abb6fc79473bdaccd19f
TongguangZhang <tonggz250@gmail.com>
2023-10-10 23:09:18 -0400

    initialized server, db connection

---
+++ server/server.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const fs = require('fs')
+const mongoose = require('mongoose')
+const workout_routes = require('./routes/workouts')
+
+//app
+const app = express()
+
+//middleware
+app.use(express.json())
+
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+    next()
+})
+
+//routes
+app.use('/api/workouts', workout_routes)
+
+
+const configData = fs.readFileSync("config.json");
+const config = JSON.parse(configData);
+
+mongoose.connect(config.dbURI)
+    .then(() => {
+        app.listen(config.port, () => {
+            console.log(`Listening on port ${config.port}`)
+        })        
+    })
+    .catch((error) => {
+        console.log(error)
+    })
+
